669932e48a7080b40d7673bc0a0a3e87a300457f
Mollier Yoann <yoann.mollier@gmail.com>
2025-08-20 23:20:53 +0200

    delete guibox

--- inc/gui.h
+++ inc/gui.h
@@ -8,114 +8,14 @@
 auto constexpr BOX_LAYER = 2.f;
 namespace SSS
 {
-	struct GUI_Layout {
+	struct GUI_Layout 
+	{
 		int32_t _ID = 0;
 
 		SSS::TR::Format _fmt;
 		int _marginh = 0, _marginv = 0;
 	};
-
-
-	class SceneGraph 
-	{
-	public:
-		SceneGraph();
-		~SceneGraph();
-	private:
-
-	};
 }
-
-
-class Box_GUI;
-
-class Plane : public SSS::GL::PlaneTemplate<Plane> {
-	friend class SharedClass;
-protected:
-	virtual glm::mat4 _getTranslationMat4() const override;
-private:
-	Plane() = default;
-	glm::vec3 _offset;
-	SSS::SharedClass<Box_GUI>::Weak _parent;
-public:
-	inline void setBox(SSS::SharedClass<Box_GUI>::Weak Box_GUI) { _parent = Box_GUI; };
-	inline auto getBox() const noexcept { return _parent.lock(); };
-	inline glm::vec3 getOffset() const noexcept { return _offset; };
-	void setOffset(glm::vec3 offset);
-};
-
-
-class Box_GUI : public SSS::Observer, public SSS::SharedClass<Box_GUI> {
-	friend class SharedClass;
-
-private:
-	Box_GUI() = default;
-public:
-	~Box_GUI();
-
-private:
-	glm::vec2 _size;
-	glm::vec3 _pos;
-	glm::vec4 _color;
-
-public:
-	inline glm::vec2 getSize() const noexcept { return _size; };
-	inline glm::vec3 getPos() const noexcept { return _pos; };
-	inline glm::vec4 getColor() const noexcept { return _color; };
-
-	bool checkCollision(glm::vec2 const& r2p, glm::vec2 const& r2s);
-	bool checkCollision(std::shared_ptr<SSS::GL::PlaneBase> plane);
-
-	glm::vec3 center() const;
-
-	//inline void setSize(glm::vec2 s) {};
-	void setPos(glm::vec3 pos);
-	inline void setZ(float z) { /*setPos(glm::vec3(_pos.x, _pos.y, z));*/ };
-	void setColor(glm::vec4 color);
-	//inline void setColor(std::string hex) { setColor(hex_to_rgb(hex)); };
-	inline void setColor(std::string hex) { setColor(SSS::RGBA_f{}.from_Hex(hex)._col); };
-
-
-	//void setSelectedCol(std::string hex);
-
-	void set_text_data(const Text_data& td);
-
-	//STATES 
-	bool isHovered() const noexcept;
-	bool isClicked() const noexcept;
-	bool isHeld() const noexcept;
-
-	bool _show_comment = false;
-	bool _show_tags = false;
-	glm::vec3 selected_color = glm::vec3(0.93f, 0.64f, 0.43f);
-
-	//Box_GUI rendering
-
-	//Initialisation of the Box_GUI and fill the model array
-	void create_box();
-private:
-	void _create_part(std::string s, const SSS::GUI_Layout& lyt, int flag = 0);
-
-	virtual void _subjectUpdate(SSS::Subject const& subjet, int event_id) override;
-	void _size_update();
-
-public:
-
-	// DATA
-	Text_data _td;
-
-	std::string _id;
-	Plane::Vector model;
-
-	std::set<std::string> link_to;
-	std::set<std::string> link_from;
-
-	static std::map<std::string, SSS::GUI_Layout> layout_map;
-
-	// DEFAULT VALUES
-	static glm::vec2 minsize;
-};
-
 
 static bool sortPlanes(std::shared_ptr<SSS::GL::PlaneBase>& a, std::shared_ptr<SSS::GL::PlaneBase>& b) {
 
