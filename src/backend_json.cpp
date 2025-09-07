6c2086e6fad647423872b5252a3c1b7ed759a386
Mollier Yoann <yoann.mollier@gmail.com>
2025-09-07 17:21:21 +0200

    remove size

--- src/backend_json.cpp
+++ src/backend_json.cpp
@@ -93,7 +93,6 @@ void from_json(const nlohmann::json& j, Box& t)
     j.at("ID").get_to(t._id);
     t.setColor(j.at("COLOR").get<glm::vec4>());
     t.setPos(j.at("POSITION"));
-    //t.setSize(j.at("SIZE"));
     j.at("TAGS").get_to(t.tags);
     j.at("LINK_TO").get_to(t.link_to);
     j.at("LINK_FROM").get_to(t.link_from);
