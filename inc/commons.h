e928e244ae6cc0c36761530cbbf0cd28400aa786
Mollier Yoann <yoann.mollier@gmail.com>
2025-09-07 15:51:25 +0200

    RGBA_f pour les randoms colors

--- inc/commons.h
+++ inc/commons.h
@@ -19,5 +19,5 @@ catch (...) {
 std::vector<std::string> split_str(std::string src, unsigned int split_len);
 
 float rand_float();
-std::string rand_color();
-glm::vec4 rand_pastel_color();
+SSS::RGBA_f rand_color();
+SSS::RGBA_f rand_pastel_color();
